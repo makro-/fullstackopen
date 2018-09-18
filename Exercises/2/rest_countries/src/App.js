94ddcd832c2700bc2e5cbcf46ea604c93d5ceefc
Marko Felin <marko.felin@cometasolutions.fi>
2018-09-18 19:27:10 +0300

    2.13

--- Exercises/2/rest_countries/src/App.js
+++ Exercises/2/rest_countries/src/App.js
@@ -29,21 +29,25 @@ class App extends React.Component {
     console.log(event.target.value)
     this.setState({ searchTerm: event.target.value })
   }
-  
+  cclicked = (event) => {
+
+    console.log(event.target.dataset.message, 'clicked')
+    this.setState({ searchTerm: event.target.dataset.message});
+  }
   getCountryData(){
 
-    const countries = this.state.countries.filter ( x => x.name.toLowerCase().includes(this.state.searchTerm));
+    const countries = this.state.countries.filter ( x => x.name.toLowerCase().includes(this.state.searchTerm.toLowerCase()));
     if(countries.length === 1)
     {
       return countries.map( c => 
-      <div key={c.name}> {c.name} {c.nativeName}<p>capital: {c.capital}</p><p>population: {c.population}</p><p><img src={c.flag} alt="flag"/></p> </div>
+      <div  key={c.name}> {c.name} {c.nativeName}<p>capital: {c.capital}</p><p>population: {c.population}</p><p><img src={c.flag} alt="flag"/></p> </div>
       
       )
       
     }
     else if(countries.length < 10)
     {
-      return   countries.map( c => <div key={c.name}> {c.name} </div>)
+      return   countries.map( c => <div data-message={c.name} onClick={this.cclicked} key={c.name}> {c.name} </div>)
     } 
     else{
       return "Too many matches, specify more "
